8a83215f367c739d1437e492b01e24c9a995db44
Greg Randall <gregrr@gmail.com>
2020-05-10 18:25:51 -0500

    added hdr code, tweaked a few things

--- timelapse-gr.py
+++ timelapse-gr.py
@@ -6,6 +6,7 @@ import exifread
 #from ftplib import FTP
 #from ftpconfig import * #credentials for ftp. done this way to keep them from getting added to git
 
+hdr=True
 
 ideal_exposure = 125
 delta_from_ideal = 10
@@ -15,8 +16,10 @@ def shoot_photo(ss,iso,w,h,filename):
     os.system('raspistill -ss '+ str(ss) +' -w '+ str(w) +' -h '+ str(h) +' -ISO '+ str(iso) +' -o '+ filename)
     return True
 
-def shoot_photo_auto(w,h,filename):
-    os.system('raspistill -w '+ str(w) +' -h '+ str(h) +' -o '+ filename)
+def shoot_photo_auto(ev,w,h,filename):
+    command = 'raspistill -ev '+ str(ev) +' -w '+ str(w) +' -h '+ str(h) +' -o '+ filename
+    #print(command)
+    os.system(command)
     return True
 
 def check_exposure (filename):
@@ -36,17 +39,39 @@ def get_exif(filename):
     f.close()
     return (float(ss_split[0])/float(ss_split[1]))
 
+def create_hdr(output_name):
+    #raspistill -ev 0 -o hdr-test/1.jpg
+    #raspistill -ev 20 -o hdr-test/2.jpg
+    #raspistill -ev -20 -o hdr-test/3.jpg
+    #pfsinme *.jpg | pfssize --maxx 600 | pfshdrcalibrate --verbose -r linear --bpp 16 | pfstmo_reinhard05 | pfsout test.jpg
+    #sudo apt install ufraw-batch
+    os.system( 'pfsinme hdr1.jpg hdr2.jpg hdr3.jpg | pfshdrcalibrate -r linear --bpp 16 | pfstmo_reinhard05 | pfsout '+ output_name )
+    return (True)
 
 start_time = time.time()
 print('testing exposures!')
-shoot_photo_auto(1296,976,'test.jpg')
+shoot_photo_auto(0,1296,976,'test.jpg')
 exposure = check_exposure('test.jpg')
 
 if (ideal_exposure + delta_from_ideal) > exposure > (ideal_exposure - delta_from_ideal):
     print('exposure is '+ str(exposure) +'/255 which is within parameters!')
     filename = str(int(time.time())) + '.jpg'
-    print('shooting photo')
-    shoot_photo_auto(3280,2464,filename)
+    
+    if hdr:
+        print('hdr enabled, shooting several photos to combine')
+        shoot_photo_auto(0,3280,2464,'hdr1.jpg')
+        print('hdr photo 1 shot')
+        shoot_photo_auto(20,3280,2464,'hdr2.jpg')
+        print('hdr photo 2 shot')
+        shoot_photo_auto(-20,3280,2464,'hdr3.jpg')
+        print('hdr photo 3 shot')
+        print('combining photos into one hdr image')
+        create_hdr(filename)
+        print('hdr creation finished')
+    else:
+        print('shooting photo')
+        shoot_photo_auto(0,3280,2464,filename)
+
     print (filename +' shot!')
     auto = True
 else:
@@ -72,13 +97,41 @@ else:
 
     if exposure > (ideal_exposure - delta_from_ideal):
         if (ideal_exposure + delta_from_ideal) < exposure: #in case we overshoot our exposure go back a half stop
-            ss_micro *= 0.75 
-            print('overshot a bit. dialing back half a stop to ' + str(round(ss_micro/1000000,2))+' seconds')
+            ss_micro *= 0.5 
+            print('overshot a bit. dialing back a bit ' + str(round(ss_micro/1000000,2))+' seconds')
         else:
             print('got it! ' + str(round(ss_micro/1000000,2))+' seconds')
         filename = str(int(time.time())) + '.jpg'
-        print('shooting photo')
-        shoot_photo(ss_micro,iso,3280,2464,filename)
+        if hdr:
+            if ss_micro>=6000000:
+                print('hdr enabled, but we maxed out exposure, so one shot will have to push iso.')
+                shoot_photo(ss_micro,iso,3280,2464,'hdr1.jpg')
+                print('hdr photo 1 shot')
+                shoot_photo(ss_micro,400,3280,2464,'hdr2.jpg')
+                print('hdr photo 2 shot')
+                shoot_photo(ss_micro/2,iso,3280,2464,'hdr3.jpg')
+                print('hdr photo 3 shot')
+                print('combining photos into one hdr image')
+                create_hdr(filename)
+                print('hdr creation finished')
+            else:
+                print('hdr enabled, shooting several photos to combine')
+                shoot_photo(ss_micro,iso,3280,2464,'hdr1.jpg')
+                print('hdr photo 1 shot')
+                if ss_micro*2>=6000000: #if the longer hdr shutter speed is greater than 6 seconds, we'll just cap it at 6 seconds
+                    shoot_photo(6000000,iso,3280,2464,'hdr2.jpg')
+                else:
+                    shoot_photo(ss_micro*2,iso,3280,2464,'hdr2.jpg')
+                print('hdr photo 2 shot')
+                shoot_photo(ss_micro/2,iso,3280,2464,'hdr3.jpg')
+                print('hdr photo 3 shot')
+                print('combining photos into one hdr image')
+                create_hdr(filename)
+                print('hdr creation finished')
+        else:
+            print('shooting photo')
+            shoot_photo(ss_micro,iso,3280,2464,filename)
+
         print (filename +' shot!')
     else:
         print('need to bump iso :(')
@@ -91,22 +144,44 @@ else:
                 print('workable iso discovered ' + str(iso))
                 break
         #if the loop ends without finding an iso, it'll just shoot a shot with the highest iso.
+        filename = str(int(time.time())) + '.jpg'
         if iso==800:
             print('highest iso & longest shutter speed hit. going to shoot a photo anyway')
 
-        filename = str(int(time.time())) + '.jpg'
-        print('shooting photo')
-        shoot_photo(ss_micro,iso,3280,2464,filename)
-        print (filename +' shot!')
+        if hdr:
+            print('hdr enabled, shooting several photos to combine')
+            shoot_photo(ss_micro,iso,3280,2464,'hdr1.jpg')
+            print('hdr photo 1 shot')
+            shoot_photo(ss_micro/2,iso,3280,2464,'hdr2.jpg')
+            print('hdr photo 2 shot')
+            if iso*2<800: #if we can get away with it, we'll increase the iso for one of the HDR shots
+                shoot_photo(ss_micro,iso*2,3280,2464,'hdr3.jpg')
+            else: #if we already hit 800 iso, then we'll just shoot an additional shot to help average iso noise away
+                print('iso already maxed out, we will shoot an extra photo for noise reduction')
+                shoot_photo(ss_micro,iso,3280,2464,'hdr3.jpg')
+            print('hdr photo 3 shot')
+            print('combining photos into one hdr image')
+            create_hdr(filename)
+            print('hdr creation finished')
+        else:
+            print('shooting photo')
+            shoot_photo(ss_micro,iso,3280,2464,filename)
+            print (filename +' shot!')
     auto = False
 
 end_time=time.time()
 f=open("timelapse-log.txt", "a+")
 timestamp = dateTimeObj = datetime.now()
-if auto:
-    f.write(str(timestamp) + " - with automatic exposure. it took " + str( (end_time-start_time)/60 ) +' decimal minutes\n')
+
+if hdr:
+    hdr_note='hdr was enabled. '
 else:
-     f.write(str(timestamp) + " - with manual exposure - " + str(round(ss_micro/1000000,2)) +" seconds at " + str(iso) +" iso. it took " + str( (end_time-start_time)/60 ) +' decimal minutes\n')
+    hdr_note=''
+
+if auto:
+    f.write(str(timestamp) + ' - with automatic exposure. ' + hdr_note + 'it took ' + str( (end_time-start_time)/60 ) +' decimal minutes\n')
+else:
+     f.write(str(timestamp) + ' - with manual exposure. '+ hdr_note + str(round(ss_micro/1000000,2)) +" seconds at " + str(iso) +" iso. it took " + str( (end_time-start_time)/60 ) +' decimal minutes\n')
 f.close()
 print('finally done. everything took ' + str( (end_time-start_time)/60 ) +' decimal minutes')
 
